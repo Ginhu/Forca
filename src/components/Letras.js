e72abcf8bd59595d5892f9a5fd5f471d946b7d9c
Sergio Beppu <beppu.sergio@gmail.com>
2023-02-27 09:24:49 -0300

    alterações para correção automatica

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -35,7 +35,7 @@ export default function Letras(props) {
 
     return (
         <div>
-            <button onClick={()=>escolheLetra(props.letra)} disabled={props.habilitar.includes(props.letra) ? true : false } data-test="letter">{props.letra.toUpperCase()}</button>
+            <button onClick={()=>escolheLetra(props.letra)} disabled={props.habilitar.includes(props.letra) ? true : false} className={props.habilitar.includes(props.letra) ? "" : "botaoHabilitado"} data-test="letter">{props.letra.toUpperCase()}</button>
         </div>
     )
 }
